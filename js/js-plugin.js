db27b234c069d5c48432f6a0c2a74864c252746f
iryna <irinagosteva2014@gmail.com>
2017-06-11 17:10:41 +0300

    done

---
+++ js/js-plugin.js
@@ -0,0 +1,15 @@
+;(function ($) {
+	var header = $("header");
+
+	$(window).on("scroll", function(){
+
+		if ($(this).scrollTop() > 20){
+			header.addClass("on-scroll");
+		} else{
+			header.removeClass("on-scroll");
+		}
+
+		// console.log($(this).scrollTop());
+
+	})
+})(jQuery);
